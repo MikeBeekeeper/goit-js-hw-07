22e055b10b2ec161cf8c572e5fa2e39efb7fa535
MikeBeekeeper <mike.rotar.91@gmail.com>
2022-12-28 15:14:13 +0200

    first attempt

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,4 +1,45 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+const galeryListOfImagesEl = document.querySelector('.gallery');
+const imageItemsMarkup = createImageMarkup(galleryItems);
+
+function returnUrlOfBiggestImg(evt) {
+    if (evt.target.nodeName !== 'IMG') {
+        return
+     }
+    const bigImg = basicLightbox.create(`
+     <img src="${evt.target.dataset.source}" width="800" height="600">
+  `) 
+    bigImg.show();
+  
+    document.addEventListener('keydown', event => {
+        if(event.code === 'Escape') {
+            bigImg.close();
+        }
+    })
+    
+}
+
+galeryListOfImagesEl.addEventListener('click', returnUrlOfBiggestImg)
+
+function createImageMarkup(items) {
+      return items.map( item =>
+      `
+      <div class="gallery__item">
+      <a class="gallery__link" href="${item.original}" onclick="return false">
+        <img
+      class="gallery__image"
+      src="${item.preview}"
+      data-source="${item.original}"
+      alt="${item.description}"
+        />
+</a>
+</div>
+    `).join('')
+  }
+galeryListOfImagesEl.innerHTML = imageItemsMarkup;
+
+
+
+
+
--- src/js/02-lightbox.js
+++ src/js/02-lightbox.js
@@ -1,4 +1,33 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+const galeryListOfImagesEl = document.querySelector('.gallery');
+const imageItemsMarkup = createImageMarkup(galleryItems);
+
+function returnUrlOfBiggestImg(evt) {
+    if (evt.target.nodeName !== 'IMG') {
+        return
+    }
+    var lightbox = new SimpleLightbox(".gallery a", {
+    captionsData: "alt",
+    captionDelay: 250,
+    })
+    lightbox.open() 
+}
+
+galeryListOfImagesEl.addEventListener('click', returnUrlOfBiggestImg)
+
+function createImageMarkup(items) {
+      return items.map( item =>
+      `
+      <a class="gallery__link" href="${item.original}" onclick="return false">
+        <img
+      class="gallery__image"
+      src="${item.preview}"
+      data-source="${item.original}"
+      alt="${item.description}"
+        />
+</a>
+    `).join('')
+  }
+galeryListOfImagesEl.innerHTML = imageItemsMarkup;
+
